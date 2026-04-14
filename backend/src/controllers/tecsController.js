35704af5f54719d7c2fbdf4e88a5970e4b2639d7
Alvaro <alvarofdezg@proton.me>
2026-04-14 16:23:20 +0200

    Encriptado de contraseña en login y register

--- backend/src/controllers/tecsController.js
+++ backend/src/controllers/tecsController.js
@@ -1,4 +1,5 @@
 const tecsService = require("../services/tecsService.js");
+const bcrypt = require("bcrypt");
 
 const getAllTecs = async (req, res) => {
     const tecs = await tecsService.getAllTecs();
@@ -17,29 +18,75 @@ const registerTec = async (req, res) => {
         });
     }
 
-    const createdUser = await tecsService.registerTec(user);
+    try {
+        const hashpassword = await bcrypt.hash(user.password, 10);
+        user.password = hashpassword;
+        const createdUser = await tecsService.registerTec(user);
 
-    if (!createdUser) {
-        return res.status(500).json({
+        if (!createdUser) {
+            return res.status(500).json({
+                error: "Error en la base de datos",
+                msg: "No se ha podido registrar al usuario"
+            });
+        }
+
+
+        res.status(200).json({
+            message: "Usuario creado correctamente",
+            user: {
+                nombre: createdUser.nombre,
+                apellidos: createdUser.apellidos,
+                email: createdUser.mail,
+                centro: createdUser.centro,
+                tipo: createdUser.tipo
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
             error: "Error en la base de datos",
-            msg: "No se ha podido registrar al usuario"
+            errores: [error.message]
+        });
+    }
+}
+
+const findTecMail = async (req, res) => {
+    const user = req.body;
+
+    if (!user.email || !user.password) {
+        return res.status(400).json({
+            error: "Falta alguno de los campos requeridos"
+        });
+    }
+    const loggedTec = await tecsService.findTecMail(user.email);
+
+    if (!loggedTec) {
+        return res.status(404).json({
+            error: "Usuario no registrado",
         });
     }
 
+    const isValid = await bcrypt.compare(user.password, loggedTec.password);
+
+    if (!isValid) {
+        return res.status(400).json({
+            error: "Usuario o contraseña incorrecta",
+        });
+    }
 
     res.status(200).json({
-        message: "Usuario creado correctamente",
+        message: "Usuario logeado correctamente",
         user: {
-            nombre: createdUser.nombre,
-            apellidos: createdUser.apellidos,
-            email: createdUser.mail,
-            centro: createdUser.centro,
-            tipo: createdUser.tipo
+            nombre: loggedTec.nombre,
+            apellidos: loggedTec.apellidos,
+            email: loggedTec.mail,
+            centro: loggedTec.centro,
+            tipo: loggedTec.tipo
         }
     });
 }
 
 module.exports = {
     getAllTecs,
-    registerTec
+    registerTec,
+    findTecMail
 }
