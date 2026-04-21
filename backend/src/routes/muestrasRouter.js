8edc5d386bc7857e974b6929b1e07ea12c9fab21
Alvaro <alvarofdezg@proton.me>
2026-04-21 16:22:02 +0200

    Endpoint de obtención de muestras

--- backend/src/routes/muestrasRouter.js
+++ backend/src/routes/muestrasRouter.js
@@ -3,5 +3,7 @@ const muestrasRouter = express.Router();
 const muestraController = require("../controllers/muestraController.js");
 
 muestrasRouter.get("/" , muestraController.getAllMuestras);
+muestrasRouter.get("/:id_cas" , muestraController.getAllMuestrasCassete);
+muestrasRouter.get("/muestra/:id_muestra" , muestraController.getMuestraId);
 
 module.exports = muestrasRouter;
