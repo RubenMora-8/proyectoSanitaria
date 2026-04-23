435b475cc190e25af3c0c54672364c1ed0521e8d
Laura Cespedes Berdonces <raul.cespedesberdonces@riberadeltajo.es>
2026-04-23 17:04:53 +0200

    Update tecsRouter.js

--- backend/src/routes/tecsRouter.js
+++ backend/src/routes/tecsRouter.js
@@ -3,16 +3,19 @@ const tecsRouter = express.Router();
 const tecsController = require("../controllers/tecsController.js");
 const authMiddleware = require("../middleware/authMiddleware.js");
 
-tecsRouter.get("/" , tecsController.getAllTecs);
 
 // Rutas publicas
 
 tecsRouter.post("/login" , tecsController.loginTec);
 tecsRouter.post("/register" , tecsController.registerTec);
-tecsRouter.delete("/:id" , tecsController.deleteTec);
+// tecsRouter.post(/"password-recovery" , tecsController.passwordrecoveryTec);
 
 // Rutas token
 
+// Admin
+tecsRouter.get("/", authMiddleware.checkToken, authMiddleware.checkAdmin, tecsController.getAllTecs);
+tecsRouter.delete("/:id" , authMiddleware.checkToken, authMiddleware.checkAdmin, tecsController.deleteTec);
 
+// Usuario
 
 module.exports = tecsRouter;
